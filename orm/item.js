a887e14c46ca3151c146ce24d249b3151a424848
Jeremy Darling <jeremy.darling@pearson.com>
2014-10-09 11:48:28 -0500

    Add parent_id back to model

--- orm/item.js
+++ orm/item.js
@@ -2,6 +2,7 @@ var ORM = require('prodio-orm');
 
 var Item = new ORM('item', {
   project_id: ORM.ID(),
+  parent_id: ORM.Optional(ORM.ID()),
   name: ORM.String(),
   description: ORM.String(),
   version: ORM.Semver(),
