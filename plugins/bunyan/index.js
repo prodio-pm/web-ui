ec19a9dabe602ba2e7334e2a8e0cfb44efbe4948
Jeremy Darling <jeremy.darling@pearson.com>
2014-10-24 12:38:27 -0500

    Logger bug fixes

--- plugins/bunyan/index.js
+++ plugins/bunyan/index.js
@@ -8,6 +8,9 @@ try{
     throw new Error('Bunyan logger requires bunyan.  Use "npm install bunyan" to resolve');
   }
 }
+if(!logger.child){
+  throw new Error('Bunyan logger requires bunyan.  Use "npm install bunyan" to resolve');
+}
 var utils = require('../../lib/utils');
 var noop = function(){};
 
@@ -15,55 +18,53 @@ var LOG_LEVELS = ['trace', 'debug', 'info', 'warn', 'error', 'fatal'];
 
 var logEvent = function logEvent(ctx, data, request){
   var obj = {};
-  var msg = '';
   var type = typeof(data.data);
-
-  obj.request = obj.request||{};
-  if(ctx.includePayload && request && request.payload){
-    obj.request.payload = request.payload;
-  }
-
-  if(ctx.includeHeaders){
-    obj.request.headers = request.headers;
-  }
-
-  if(ctx.includeResponse && request.response){
-    var res = {}, include = false;
-    if(request.response.statusCode){
-      res.statusCode = request.response.statusCode;
-      include = true;
-    }
-    if(request.response.headers){
-      res.headers = request.response.headers;
-      include = true;
-    }
-    if(request.response.source){
-      try{
-        res.payload = typeof(request.response.source)==='object'?
-          JSON.parse(JSON.stringify(request.response.source)):
-          request.response.source;
-          include = true;
-      }catch(e){
-        //res.payload = request.response.source;
-      }
-    }
-    if(include){
-      obj.response = res;
-    }
-  }
-
-  if(ctx.includeTags){
-    obj.tags = ctx.joinTags ? data.tags.join(ctx.joinTags) : data.tags;
-  }
 
   if(request){
     obj.req_id = request.id;
     obj.url = request.url.path;
     obj.method = request.method.toUpperCase();
+
+    obj.request = obj.request||{};
+    if(ctx.includePayload && request && request.payload){
+      obj.request.payload = request.payload;
+    }
+
+    if(ctx.includeParams && request && request.params && Object.keys(request.params).length){
+      obj.request.params = request.params;
+    }
+
+    if(ctx.includeHeaders && request && request.headers){
+      obj.request.headers = request.headers;
+    }
+
+    if(ctx.includeResponse && request && request.response){
+      var res = {}, include = false;
+      if(request.response.statusCode){
+        res.statusCode = request.response.statusCode;
+        include = true;
+      }
+      if(request.response.headers){
+        res.headers = request.response.headers;
+        include = true;
+      }
+      if(request.response.source){
+        try{
+          res.payload = typeof(request.response.source)==='object'?
+            JSON.parse(JSON.stringify(request.response.source)):
+            request.response.source;
+            include = true;
+        }catch(e){
+        }
+      }
+      if(include){
+        obj.response = res;
+      }
+    }
   }
 
-  if(type==='string'){
-    msg = data.data;
+  if(ctx.includeTags){
+    obj.tags = ctx.joinTags ? data.tags.join(ctx.joinTags) : data.tags;
   }
 
   if(type!=='string'&&type!=='undefined'&&ctx.includeData){
@@ -74,7 +75,11 @@ var logEvent = function logEvent(ctx, data, request){
     return;
   }
 
-  ctx.log[ctx.level](obj, msg);
+  if(type==='string'){
+    return ctx.log[ctx.level](obj, data.data);
+  }
+
+  ctx.log[ctx.level](obj);
 };
 
 var setDefault = function setDefault(on, member, value){
@@ -90,12 +95,13 @@ var bunyan = function bunyan(server, options, next){
   var log = options.logger;
   var handler = options.handler||noop;
 
-  setDefault(options, 'includeTags', true);
+  setDefault(options, 'includeTags', false);
   setDefault(options, 'includeData', true);
   setDefault(options, 'mergeData', false);
   setDefault(options, 'skipUndefined', false);
   setDefault(options, 'joinTags', false);
   setDefault(options, 'includePayload', true);
+  setDefault(options, 'includeParams', true);
   setDefault(options, 'includeResponse', true);
   setDefault(options, 'includeHeaders', true);
 
@@ -104,12 +110,22 @@ var bunyan = function bunyan(server, options, next){
     request.log = function(){
       _log.apply(request, arguments);
     };
-    request.bunyan = log.child({req_id: request.id});
+    request.bunyan = log.child({req_id: request.id}, true);
     LOG_LEVELS.forEach(function(level){
       request.log[level]=request.bunyan[level].bind(request.bunyan);
     });
     next();
   });
+
+  server.log = (function(){
+    var _super = server.log;
+    return function(){
+      var args = Array.prototype.slice.apply(arguments);
+      var type = args.shift();
+      var msg = args.join('\t');
+      _super.call(server, type, msg);
+    };
+  })();
 
   server.events.on('log', function(data, tags){
     var ctx = {
@@ -120,6 +136,7 @@ var bunyan = function bunyan(server, options, next){
       mergeData: options.mergeData,
       skipUndefined: options.skipUndefined,
       includePayload: options.includePayload,
+      includeParams: options.includeParams,
       includeResponse: options.includeResponse,
       includeHeaders: options.includeHeaders,
       joinTags: options.joinTags
@@ -139,6 +156,7 @@ var bunyan = function bunyan(server, options, next){
       mergeData: options.mergeData,
       skipUndefined: options.skipUndefined,
       includePayload: options.includePayload,
+      includeParams: options.includeParams,
       includeResponse: options.includeResponse,
       includeHeaders: options.includeHeaders,
       joinTags: options.joinTags
